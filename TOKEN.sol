1897c1b57e24bf7d06f6cbc422b4a1a161bba6f3
chainswap <chainswap@pm.me>
2021-04-23 06:38:36 +0800

    TOKENING

--- TOKEN.sol
+++ TOKEN.sol
@@ -440,11 +440,11 @@ contract ERC20UpgradeSafe is Initializable, ContextUpgradeSafe, IERC20 {
     using SafeMath for uint256;
     using Address for address;
 
-    mapping (address => uint256) private _balances;
+    mapping (address => uint256) internal _balances;
 
     mapping (address => mapping (address => uint256)) private _allowances;
 
-    uint256 private _totalSupply;
+    uint256 internal _totalSupply;
 
     string private _name;
     string private _symbol;
@@ -1388,4 +1388,59 @@ contract TOKEN is MappableToken {
 		_mint(public_,           1_000_000 * 10 ** uint256(decimals()));
 		_mint(airdrop_,          1_000_000 * 10 ** uint256(decimals()));
 	}
+}
+
+
+contract TOKENING is ERC20UpgradeSafe, Configurable {
+    using SafeERC20 for IERC20;
+    
+    address public  token;
+    uint    public  firstTime;
+    uint    public  firstRatio;
+    uint    public  begin;
+    uint    public  end;
+    mapping (address => uint) internal _beginBalances;
+	
+	function __TOKENING_init(address governor_) public initializer {
+		__Governable_init_unchained(governor_);
+        __Context_init_unchained();
+		__ERC20_init_unchained("ChainSwap.com TOKEN locking", "TOKENING");
+		_setupDecimals(18);
+	}
+	
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+        if(from != address(0) && to != address(0)) {
+            uint beginAmount = amount.mul(_beginBalances[from]).div(_balances[from]);
+            _beginBalances[from] = _beginBalances[from].sub(beginAmount);
+            _beginBalances[to] = _beginBalances[to].add(beginAmount);
+        }else if(to != address(0))                                              // _mint
+            _beginBalances[to] = _beginBalances[to].add(amount);
+    }
+    
+	function startUnlock(address _token, address recipient, uint _firstTime, uint _firstRatio, uint _begin, uint _end) external governance {
+	    token = _token;
+		_setupDecimals(ERC20UpgradeSafe(token).decimals());
+	    _mint(recipient, IERC20(token).balanceOf(address(this)).sub(_totalSupply));
+
+	    firstTime = _firstTime;
+	    firstRatio = _firstRatio;
+	    begin = _begin;
+	    end = _end;
+	}
+	
+    function unlockCapacity(address holder) public view returns (uint) {
+        if(now < firstTime)
+            return 0;
+        return _balances[holder].sub(_beginBalances[holder].mul(uint(1e18).sub(firstRatio)).div(1e18).mul(end.sub(Math.min(Math.max(begin, now), end))).div(end.sub(begin)));
+    }
+    
+    function unlock() public {
+        uint amount = unlockCapacity(msg.sender);
+        _burn(msg.sender, amount);
+        IERC20(token).safeTransfer(msg.sender, amount);
+    }
+    
+    fallback() external {
+        unlock();
+    }
 }
